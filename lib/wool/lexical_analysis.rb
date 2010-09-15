41a4230cfb1478825bbcc037d208d27cf46501aa
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-09-15 06:00:16 -0400

    Using the advice module to clean up lexical_analysis.rb. Gotta add some specs for it.

---
+++ lib/wool/lexical_analysis.rb
@@ -0,0 +1,44 @@
+module Wool
+  module LexicalAnalysis
+    def self.included(klass)
+      klass.__send__(:extend, ClassMethods)
+      klass.__send__(:include, InstanceMethods)
+    end
+    
+    module ClassMethods
+      extend Advice
+      
+      def lex(body)
+        Ripper::Lexer.new(body).lex
+      end
+      
+      def prelex(body, *args)
+        body === Array ? [body, *args] : [lex(body), *args]
+      end
+      
+      def has_keyword?(body, keyword)
+        body.find {|tok| tok[1] == :on_kw && tok[2] == keyword}
+      end
+      argument_advice :has_keyword?, :prelex
+      
+      def has_token?(body, token, text=nil)
+        body.find {|tok| tok[1] == token}
+      end
+      argument_advice :has_token?, :prelex
+    end
+    
+    module InstanceMethods
+      def lex
+        @lexed ||= self.class.lex(self.body)
+      end
+      
+      def has_token?(body = self.body, token)
+        self.class.has_token?(lex, token)
+      end
+      
+      def has_keyword?(body = self.body, keyword)
+        self.class.has_keyword?(lex, keyword)
+      end
+    end
+  end
+end
