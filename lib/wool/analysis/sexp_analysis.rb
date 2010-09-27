6c296c51ee3ca6f48e29caccf9c8816f100ac98d
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-09-27 13:53:42 -0400

    Added specs for sexp_analysis.

--- lib/wool/analysis/sexp_analysis.rb
+++ lib/wool/analysis/sexp_analysis.rb
@@ -4,16 +4,19 @@ module Wool
   module SexpAnalysis
     # Parses the given text.
     #
-    # @param [String] body (self.body) The text to lex
-    # @return [Array<Array<Integer, Integer>, Symbol, String>] A set of tokens
-    #   in Ripper's result format. Each token is an array of the form:
-    #   [[1, token_position], token_type, token_text]. I'm not exactly clear on
-    #   why the 1 is always there. At any rate - the result is an array of those
-    #   tokens.
+    # @param [String] body (self.body) The text to parse
+    # @return [Sexp, NilClass] the sexp representing the input text.
     def parse(body = self.body)
       Ripper.sexp(body)
     end
     
+    # Finds all sexps of the given type in the given Sexp tree.
+    #
+    # @param [Symbol] type the type of sexp to search for
+    # @param [Sexp] tree (self.parse(self.body)) The tree to search in. Leave
+    #   blank to search the entire body.
+    # @return [Array<Sexp>] all sexps in the input tree (or whole body) that
+    #   are of the given type.
     def find_sexps(type, tree = self.parse(self.body))
       result = tree[0] == type ? [tree] : []
       tree.each do |node|
---
+++ spec/analysis_specs/sexp_analysis_spec.rb
@@ -0,0 +1,30 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe SexpAnalysis do
+  before do
+    @class = Class.new do
+      include SexpAnalysis
+      attr_accessor :body
+      def initialize(body)
+        self.body = body
+      end
+    end
+  end
+
+  context '#parse' do
+    it 'parses its body' do
+      @class.new('a').parse.should ==
+          [:program, [[:var_ref, [:@ident, "a", [1, 0]]]]]
+    end
+  end
+
+  context '#find_sexps' do
+    it 'searches its body' do
+      @class.new('a + b').find_sexps(:binary).should_not be_empty
+    end
+
+    it 'returns an empty array if no sexps are found' do
+      @class.new('a + b').find_sexps(:rescue).should be_empty
+    end
+  end
+end
