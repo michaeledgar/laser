e05aaaaba953de0160a83cf7ce8c011cf582aee4
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-12-15 07:48:23 -0500

    Specced the global scope. Almost home free to do features!

--- lib/wool/analysis/symbol.rb
+++ lib/wool/analysis/symbol.rb
@@ -2,7 +2,7 @@ module Wool
   module SexpAnalysis
     # This class represents a Symbol in Ruby. It may have a known protocol (type),
     # class, value (if constant!), and a variety of other details.
-    class Symbol < Struct.new(:protocol, :class, :value)
+    class Symbol < Struct.new(:protocol, :class_used, :value)
       include Comparable
       
     end
