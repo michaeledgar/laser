34aac0ad292fe600d9299e0ea94ba4f36e160d34
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-12-15 07:43:12 -0500

    Finished covering the proto registry.

--- spec/analysis_specs/protocol_registry_spec.rb
+++ spec/analysis_specs/protocol_registry_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'ostruct'
 
 describe SexpAnalysis::ProtocolRegistry do
   before(:each) do
@@ -24,13 +25,34 @@ describe SexpAnalysis::ProtocolRegistry do
   
   context '#add_class_protocol' do
     it 'adds a protocol to the main protocol list, and adds a shortcut in the class map' do
-      require 'ostruct'
       x = OpenStruct.new
       x.class_used = OpenStruct.new
       x.class_used.path = 'SuperPath'
       SexpAnalysis::ProtocolRegistry.add_class_protocol x
       SexpAnalysis::ProtocolRegistry.protocols.should include(x)
       SexpAnalysis::ProtocolRegistry.class_protocols['SuperPath'].should == x
+    end
+  end
+  
+  context '#[]' do
+    it 'looks up quick queries by class path' do
+      x = OpenStruct.new
+      x.class_used = OpenStruct.new
+      x.class_used.path = 'SuperPath'
+      SexpAnalysis::ProtocolRegistry.add_class_protocol x
+      SexpAnalysis::ProtocolRegistry['SuperPath'].should == [x]
+    end
+  end
+  
+  context '#query' do
+    context 'with :class_path specified' do
+      it 'finds the classes in the registry with the given path' do
+        x = OpenStruct.new
+        x.class_used = OpenStruct.new
+        x.class_used.path = 'SuperPath'
+        SexpAnalysis::ProtocolRegistry.add_class_protocol x
+        SexpAnalysis::ProtocolRegistry.query(:class_path => 'SuperPath').should == [x]
+      end
     end
   end
 end
