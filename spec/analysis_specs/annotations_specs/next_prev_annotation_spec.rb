11ed3cb76503e865fb0b284029692f56acd16a78
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-12-14 22:22:03 -0500

    Basic specs for next/prev annotations.

---
+++ spec/analysis_specs/annotations_specs/next_prev_annotation_spec.rb
@@ -0,0 +1,48 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'set'
+describe Annotations::NextPrevAnnotation do
+  it 'adds the #next and #prev methods to Sexp' do
+    SexpAnalysis::Sexp.instance_methods.should include(:next)
+    SexpAnalysis::Sexp.instance_methods.should include(:prev)
+  end
+  
+  it 'adds next and prevs to each node with a toy example' do
+    tree = SexpAnalysis::Sexp.new([:abc, SexpAnalysis::Sexp.new([:def, 1, 2]),
+                                   SexpAnalysis::Sexp.new([:zzz, SexpAnalysis::Sexp.new([:return]), 
+                                                           "hi", SexpAnalysis::Sexp.new([:silly, 4])])])
+    Annotations::NextPrevAnnotation::Annotator.new.annotate!(tree)
+    tree[1].prev.should == nil
+    tree[1].next.should == tree[2]
+    tree[2].prev.should == tree[1]
+    tree[2].next.should == nil
+    tree[2][1].prev.should == nil
+    tree[2][1].next.should == tree[2][2]
+    tree[2][3].prev.should == tree[2][2]
+    tree[2][3].next.should == nil
+  end
+  
+  # This will actually verify that every node in the tree has a
+  # proper parent set. It's a complex, but thorough test.
+  it 'adds next and prevs to each node with a real-world parse result' do
+    tree = SexpAnalysis::Sexp.new(Ripper.sexp('x = proc {|x, *rst, &blk| p x ** rst[0]; blk.call(rst[1..-1])}'))
+    tree.next.should == nil
+    tree.prev.should == nil
+    visited = Set.new
+    to_visit = tree.children
+    while to_visit.any?
+      todo = to_visit.pop
+      next unless is_sexp?(todo)
+      
+      todo.prev.next.should == todo if is_sexp?(todo.prev)
+      todo.next.prev.should == todo if is_sexp?(todo.next)
+
+      visited << todo
+      case todo[0]
+      when Array
+        to_visit.concat todo
+      when Symbol
+        to_visit.concat todo.children
+      end
+    end
+  end
+end
--- spec/analysis_specs/annotations_specs/spec_helper.rb
+++ spec/analysis_specs/annotations_specs/spec_helper.rb
@@ -1,1 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+def is_sexp?(sexp)
+  SexpAnalysis::Sexp === sexp
+end
