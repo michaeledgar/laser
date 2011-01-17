fa0c06ed1f848f29d83a8fb9c1a54a50234a7a68
Michael Edgar <michael.j.edgar@dartmouth.edu>
2011-01-16 19:17:54 -0500

    Class Estimate logic for refining upper bounds.

--- lib/wool/analysis/class_estimate.rb
+++ lib/wool/analysis/class_estimate.rb
@@ -6,16 +6,35 @@ module Wool
     # from types! This class handles only inferring the classes of things.
     class ClassEstimate
       extend ModuleExtensions
-      attr_accessor :upper_bound, :lower_bound
+      attr_reader :upper_bound, :lower_bound
       def initialize(upper_bound = ClassRegistry['Object'], lower_bound = nil)
-        self.upper_bound = upper_bound
-        self.lower_bound = lower_bound
+        @upper_bound = upper_bound
+        @lower_bound = lower_bound
       end
       
       def exact?
         upper_bound == lower_bound
       end
       opposite_method :inexact?, :exact?
+      
+      def upper_bound=(new_upper_bound)
+        # Three cases relative to the old upper bound:
+        # 1. new upper bound is a superclass of the current one. No new information.
+        # 2. new upper bound is a subclass of the current upper bound. Refine the upper bound.
+        # 3. new upper bound is neither a superclass nor a subclass. Type error.
+        #
+        # Also note: if the new upper bound crosses the lower bound, then 
+        if upper_bound.superset.include?(new_upper_bound)  # no-op
+        elsif upper_bound.subset.include?(new_upper_bound)
+          @upper_bound = new_upper_bound
+          if lower_bound && lower_bound.proper_subset.include?(upper_bound)
+            raise "Type error - upper bound #{upper_bound} crosses lower bound #{lower_bound}"
+          end
+        else
+          raise "Type error - upper bound #{new_upper_bound} is not related "+
+                " to current upper bound #{upper_bound}"
+        end
+      end
     end
   end
 end
--- spec/analysis_specs/class_estimate_spec.rb
+++ spec/analysis_specs/class_estimate_spec.rb
@@ -22,4 +22,29 @@ describe SexpAnalysis::ClassEstimate do
       ClassEstimate.new(ClassRegistry['Numeric'], ClassRegistry['Integer']).should be_inexact
     end
   end
+  
+  describe '#upper_bound=' do
+    before do
+      @estimate = ClassEstimate.new(ClassRegistry['Integer'])
+    end
+
+    it 'does nothing if the new upper bound is a superclass of the old upper bound' do
+      @estimate.upper_bound = ClassRegistry['Numeric']
+      @estimate.upper_bound.should == ClassRegistry['Integer']
+    end
+
+    it 'refines the estimate if the new upper bound is a subclass of the old upper bound' do
+      @estimate.upper_bound = ClassRegistry['Fixnum']
+      @estimate.upper_bound.should == ClassRegistry['Fixnum']
+    end
+    
+    it 'raises if the new upper bound crosses the lower bound' do
+      estimate = ClassEstimate.new(ClassRegistry['Numeric'], ClassRegistry['Integer'])
+      expect { estimate.upper_bound = ClassRegistry['Fixnum'] }.to raise_error(Exception)
+    end
+    
+    it 'raises if the new upper bound is unrelated to the old upper bound' do
+      expect { @estimate.upper_bound = ClassRegistry['String'] }.to raise_error(Exception)
+    end
+  end
 end
