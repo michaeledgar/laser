5745d2b11e917738a5102ce0e34526b454986308
Michael Edgar <michael.j.edgar@dartmouth.edu>
2011-01-25 14:10:55 -0500

    Comment annotation working in a small way!

--- lib/wool/analysis/annotations/comment_attachment_annotation.rb
+++ lib/wool/analysis/annotations/comment_attachment_annotation.rb
@@ -5,15 +5,42 @@ module Wool
     # is easier.
     module CommentAttachmentAnnotation
       extend BasicAnnotation
-      add_property :docstring
+      add_property :comment
       
       # This is the annotator for the parent annotation.
       class Annotator
         def annotate_with_text(root, text)
           comments = extract_comments(text)
+          # For each comment:
+          #   find the first node, by DFS, that has a location *greater* than the
+          #   comment's. However, not all nodes will have locations due to Ripper
+          #   being kinda crappy.
+          # When the enumerator finishes (raises StopIteration): we can no longer
+          #   annotate. So return.
+
+          # root[1] here to ignore the spurious :program node
+          dfs_enumerator = root[1].dfs_enumerator
           comments.each do |comment|
-            
+            begin
+              cur = dfs_for_useful_node(dfs_enumerator)
+            end while (cur.source_begin <=> comment.location) == -1
+            # if we're here, we found the first node after the comment.
+            cur.comment = comment
           end
+        rescue StopIteration
+          # do nothing – this signals the end of the algorithm. If has_next made sense
+          # for enumerators, we'd use it, but it doesn't, so we just catch and return.
+        end
+        
+        # Runs the generator until we find a node we can use in the context of
+        # comment annotation: it must have a source_begin attribute (not all nodes
+        # can successfully resolve their source_begin) and it shouldn't be a mere
+        # Array, it should have an actual node type.
+        def dfs_for_useful_node(generator)
+          begin
+            cur = generator.next
+          end while cur.source_begin.nil? || !(::Symbol === cur[0])
+          cur
         end
         
         def extract_comments(text)
--- spec/analysis_specs/annotations_specs/comment_attachment_spec.rb
+++ spec/analysis_specs/annotations_specs/comment_attachment_spec.rb
@@ -4,38 +4,37 @@ describe CommentAttachmentAnnotation do
   clean_registry
   
   it 'adds the #docstring method to Sexp' do
-    Sexp.instance_methods.should include(:docstring)
+    Sexp.instance_methods.should include(:comment)
   end
   
   # [:program,
-  #  [[:assign,
-  #    [:var_field, [:@ident, "arr", [1, 0]]],
-  #    [:mrhs_new_from_args,
-  #     [[:var_ref, [:@kw, "true", [1, 6]]],
-  #      [:var_ref, [:@kw, "nil", [1, 12]]],
-  #      [:@int, "314", [1, 17]],
-  #      [:@float, "4.155", [1, 22]],
-  #      [:@CHAR, "?x", [1, 29]],
-  #      [:regexp_literal,
-  #       [[:@tstring_content, "ac", [1, 34]]],
-  #       [:@regexp_end, "/ix", [1, 36]]],
-  #      [:string_literal,
-  #       [:string_content,
-  #        [:@tstring_content, "Abc", [1, 42]],
-  #        [:string_embexpr, [[:var_ref, [:@ident, "hi", [1, 47]]]]]]],
-  #      [:dyna_symbol,
-  #       [[:@tstring_content, "hello ", [1, 55]],
-  #        [:string_embexpr, [[:var_ref, [:@ident, "there", [1, 63]]]]]]]],
-  #     [:hash,
-  #      [:assoclist_from_args,
-  #       [[:assoc_new,
-  #         [:@label, "a:", [1, 74]],
-  #         [:symbol_literal, [:symbol, [:@ident, "b", [1, 78]]]]]]]]]]]]
+  # [[:def,
+  #   [:@ident, "silly", [3, 4]],
+  #   [:paren,
+  #    [:params,
+  #     [[:@ident, "a", [3, 10]], [:@ident, "b", [3, 13]]],
+  #     nil,
+  #     nil,
+  #     nil,
+  #     nil]],
+  #   [:bodystmt, [[:void_stmt]], nil, nil, nil]],
+  #  [:class,
+  #   [:const_ref, [:@const, "A", [6, 7]]],
+  #   nil,
+  #   [:bodystmt, [[:void_stmt]], nil, nil, nil]]]]
+  
   
   it 'discovers the comments before a method declaration' do
-    input = "  # abc\n  #  def\ndef silly(a, b)\n end"
+    input = "  # abc\n  #  def\ndef silly(a, b)\n end\n # a class\n class A; end"
     tree = Sexp.new(Ripper.sexp(input))
+    # source location is *required* for CommentAttachment to work.
+    SourceLocationAnnotation::Annotator.new.annotate_with_text(tree, input)
     CommentAttachmentAnnotation::Annotator.new.annotate_with_text(tree, input)
     list = tree[1]
+    
+    defn = list[0]
+    defn.comment.body.should == " abc\n  def\n"
+    klass_defn = list[1]
+    klass_defn.comment.body.should == " a class\n"
   end
 end
