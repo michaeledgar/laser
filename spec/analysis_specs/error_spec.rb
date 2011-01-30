06b087eef7396bbe53d6c348849ace4e13937626
Michael Edgar <michael.j.edgar@dartmouth.edu>
2011-01-29 21:29:50 -0500

    Added a simple spec for errors

---
+++ spec/analysis_specs/error_spec.rb
@@ -0,0 +1,30 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Error do
+  it 'is a StandardError subclass' do
+    Error.ancestors.should include(StandardError)
+  end
+
+  describe '#initialize' do
+    it 'assigns a message, an AST node, and a severity' do
+      result = Error.new('msg here', [:lol], 3)
+      result.message.should == 'msg here'
+      result.ast_node.should == [:lol]
+      result.severity.should == 3
+    end
+  end
+  
+  context 'when subclassed' do
+    before do
+      @temp_class = Class.new(Error) do
+        severity 3
+      end
+    end
+    it 'has a class-level severity method for specifying a constant severity' do
+      result = @temp_class.new('a', [:hi])
+      result.message.should == 'a'
+      result.ast_node.should == [:hi]
+      result.severity.should == 3
+    end
+  end
+end
