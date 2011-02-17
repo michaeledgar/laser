6bf11625f893076b4b229f1d51af7388034f221c
Michael Edgar <michael.j.edgar@dartmouth.edu>
2011-02-16 20:57:31 -0500

    Refactored next/prev annotation into parent annotation. Rename coming.

--- lib/laser/analysis/annotations/parent_annotation.rb
+++ lib/laser/analysis/annotations/parent_annotation.rb
@@ -5,7 +5,7 @@ module Laser
     # is easier.
     # This is the annotator for the parent annotation.
     class ParentAnnotation < BasicAnnotation
-      add_property :parent
+      add_property :parent, :next, :prev
       add_computed_property :ancestors do
         case parent
         when nil then []
@@ -16,8 +16,12 @@ module Laser
       # Replaces the general node visit method with one that assigns
       # the current scope to the visited node.
       def default_visit(node)
-        node.children.select { |x| SexpAnalysis::Sexp === x }.each do |sexp|
-          sexp.parent = node
+        children = node.children
+        children.each_with_index do |elt, idx|
+          next unless SexpAnalysis::Sexp === elt
+          elt.parent = node
+          elt.next = children[idx+1]
+          elt.prev = children[idx-1] if idx >= 1
         end
         visit_children(node)
       end
